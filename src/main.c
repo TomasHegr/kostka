09f86295bd384b74850af05bbbef9ff5497a0f17
TomasHegr <hegr.tomik@seznam.cz>
2026-01-20 11:47:03 +0100

    první způsob

--- src/main.c
+++ src/main.c
@@ -1,63 +1,57 @@
-#include <stdbool.h>
-#include <stm8s.h>
 #include "main.h"
 #include "milis.h"
-//#include "delay.h"
-//#include "uart1.h"
-//#include <stdio.h>
-
-// Discovery Board
-#ifdef STM8S003
-#define LED_PORT GPIOD
-#define LED_PIN  GPIO_PIN_0
-#define BTN_PORT GPIOB
-#define BTN_PIN  GPIO_PIN_7
-#endif
-// Dero Board
-#ifdef STM8S103
-#define LED_PORT GPIOD
-#define LED_PIN  GPIO_PIN_4
-#endif
-// Discovery Board
-#ifdef STM8S105
-#define LED_PORT GPIOD
-#define LED_PIN  GPIO_PIN_0
-#endif
-// Nucleo Kit
-#ifdef STM8S208
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#endif
+#include <stdbool.h>
+#include <stm8s.h>
+// #include "delay.h"
+// #include "uart1.h"
+// #include <stdio.h>
+#include "daughterboard.h"
 
 
 void init(void)
 {
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // taktovani MCU na 16MHz
     init_milis();
-    //init_uart1();
+    // init_uart1();
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED1_PORT, DB_LED1_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED2_PORT, DB_LED2_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED4_PORT, DB_LED4_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED5_PORT, DB_LED5_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED6_PORT, DB_LED6_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED7_PORT, DB_LED7_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED8_PORT, DB_LED8_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
 
+    GPIO_Init(DB_S1_PORT,DB_S1_PIN,GPIO_MODE_IN_PU_NO_IT);
 }
 
 
 int main(void)
 {
-   
+
+    uint32_t num = 0;
+    uint8_t cube[6] ={
+        0b00100000, //1
+        0b10000001, //2
+        0b01100010, //3
+        0b11000011, //4
+        0b11100011, //5
+        0b11011011, //6
+    };
+
     
-    uint32_t time = 0;
 
     init();
 
     while (1) {
-        if (milis() - time > 333 ) {
-            REVERSE(LED); 
-            time = milis();
-            //printf("%ld\n", time);
+        if(PUSH(DB_S1)){
+            num++;
+            GPIO_Write(GPIOG, 0b00000000);           //VŠECHNO ZHASNU
+        } else{
+            GPIO_Write(GPIOG, cube[num % 6]);
         }
-        //delay_ms(333);
+
+        
     }
 }
 
